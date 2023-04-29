8c6e0481da11f4b4f82a298e1cf20daf0a21ca59
Frantisek Vidensky <fanda.vidensky@seznam.cz>
2023-04-29 14:59:29 +0200

    add examples module

--- compiler/tests/test_frag_generator.py
+++ compiler/tests/test_frag_generator.py
@@ -1,5 +1,4 @@
 import unittest
-import pathlib
 
 from antlr4 import CommonTokenStream, FileStream, ParseTreeWalker
 
@@ -7,15 +6,11 @@ from agentspeak.frag_generator import FragGenerator
 from agentspeak.asl.AgentSpeakLexer import AgentSpeakLexer
 from agentspeak.asl.AgentSpeakParser import AgentSpeakParser
 
-EXAMPLES_PATH = pathlib.Path(__file__).parent.parent.resolve() / 'examples'
-
-
-def _get_example_file_path(example_name: str, file_extension: str) -> str:
-    return (EXAMPLES_PATH / example_name / f'{example_name}.{file_extension}').as_posix()
+from examples import get_example_file_path
 
 
 def _compile(example_name: str) -> str:
-    input_stream = FileStream(_get_example_file_path(example_name, 'asl'))
+    input_stream = FileStream(get_example_file_path(example_name, 'asl'))
     lexer = AgentSpeakLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = AgentSpeakParser(stream)
@@ -29,7 +24,7 @@ def _compile(example_name: str) -> str:
 
 
 def _get_expected_output(example_name: str) -> str:
-    with open(_get_example_file_path(example_name, 'fap'), 'r') as f:
+    with open(get_example_file_path(example_name, 'fap'), 'r') as f:
         return f.read()
 
 
