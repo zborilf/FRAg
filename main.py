e885ae38d39ac993875dee88b00299b0651b7bc9
frantisekvidensky <frantisek.vidensky@kiwi.com>
2025-01-17 10:40:38 +0100

    run frag

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import os
 import glob
 import pathlib
-import tempfile
-import subprocess
 
 from PyQt6.QtGui import QFileSystemModel, QKeySequence, QShortcut
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QMessageBox
 from PyQt6.QtCore import QDir, Qt
 
-from compiler.agentspeak.compiler import compile_mas
 from gui.design import Ui_MainWindow  # Import the generated UI
 from gui.syntax.asl.highlighter import ASLSyntaxHighlighter
 from gui.syntax.mas2j.highlighter import MAS2JSyntaxHighlighter
 from gui.swipl_config import get_swipl_path
+from gui.frag_executor import FragExecutor
 
 FRAG_PATH = pathlib.Path(__file__).parent / "core"
 
@@ -48,6 +46,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Get the path to SWI-Prolog
         self.swipl_path = get_swipl_path()
+
+        # Initialize the FragExecutor
+        self.frag_executor = FragExecutor(FRAG_PATH, self.swipl_path)
 
     # Init methods
     def initialize_tree_view(self):
@@ -97,36 +98,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     # Signal handlers
     def on_run(self):
-        with tempfile.TemporaryDirectory() as dir_name:
-            try:
-                mas2j_path = pathlib.Path(self.active_config_path)
-                mas2fp_path, output_files = compile_mas(mas2j_path.as_posix(), dir_name)
-            except Exception as e:
-                QMessageBox.critical(self, "Error", f"Failed to compile MAS:\n{e}")
-                return
-
-            mas2fp_path_without_extension = mas2fp_path.with_suffix('')
-            command = ["swipl", "-l", "FragPL.pl", "-g", f"\"frag('{mas2fp_path_without_extension.as_posix()}')\"", "-g", "halt"]
-
-            try:
-                result = subprocess.run(
-                    command,
-                    cwd=FRAG_PATH.as_posix(),  # Set working directory to `core`
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    text=True
-                )
-
-                # Process the output
-                if result.returncode == 0:
-                    print(f"Success: {result.stdout}")
-                else:
-                    print(f"Error: {result.stderr}")
-                    QMessageBox.critical(self, "Error", f"FRAg execution failed:\n{result.stderr}")
-            except Exception as e:
-                QMessageBox.critical(self, "Error", f"Failed to execute Frag command:\n{e}")
-
-            a = 1
+        self.runButton.setEnabled(False)
+        try:
+            self.frag_executor.execute(self.active_config_path, self)
+        finally:
+            self.runButton.setEnabled(True)
 
 
     def on_file_selected(self, index):
