d8fa4f1652b4946e12c33f401f4d9108cdb186dc
frantisekvidensky <frantisek.vidensky@kiwi.com>
2025-02-06 13:43:34 +0100

    make compiler executable

--- compiler/main.py
+++ compiler/main.py
@@ -1,10 +1,14 @@
 import sys
-from agentspeak.compiler import compile_mas
+from pathlib import Path
+
+from .agentspeak.compiler import compile_mas
 
 
-def main(argv):
-    compile_mas(argv[1], argv[2])
+def main(mas_file, target_dir):
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+    compile_mas(mas_file, target_dir)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1], sys.argv[2])
